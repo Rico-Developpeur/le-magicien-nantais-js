b93c8a3621b707505fbafc0a2bbf89f77edc13a8
Rico-Developpeur <eric.vieira@sncf.fr>
2022-11-15 18:47:39 +0100

    add youtube, change teaser mobile

--- projet1.js
+++ projet1.js
@@ -1,22 +1,21 @@
-// transition texte teaser
-const ratio = 0.1
+// transition texte teaserTblette et teaserMobile
+const ratio = 0.1;
 const options = {
-    root: null,
-    rootMargin: "0px",
-    threshold: ratio
-}
+  root: null,
+  rootMargin: "0px",
+  threshold: ratio,
+};
 
 const handleIntersect = function (entries, observer) {
-    entries.forEach(function (entry) {
-        if (entry.intersectionRatio > ratio) {
-            entry.target.classList.add("reveal-visible")
-            observer.unobserver(entry.target)
-        }
-    })
-}
+  entries.forEach(function (entry) {
+    if (entry.intersectionRatio > ratio) {
+      entry.target.classList.add("reveal-visible");
+      observer.unobserver(entry.target);
+    }
+  });
+};
 
-const observer = new IntersectionObserver(handleIntersect, options)
+const observer = new IntersectionObserver(handleIntersect, options);
 document.querySelectorAll(".reveal").forEach(function (r) {
-    observer.observe(r)
-})
-
+  observer.observe(r);
+});
